7e7ebd27b38d88b9ef6ac49f4c01c77bab467a8b
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-09-01 00:01:16 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -10,12 +10,17 @@ import "../styles/index.css";
 
 const Home=(props)=>{
     return (
-        <div className="container-fluid">
-            <div className="row  justify-content-center">
-            <div className="col"><h1>{props.digitFour}</h1></div>
-            <div className="col"><h1>{props.digitThree}</h1></div>
-            <div className="col"><h1>{props.digitTwo}</h1></div>
-            <div className="col"><h1>{props.digitOne}</h1></div>
+        <div className="container d-flex justify-content-center">
+            <div className="row">
+            <div className="col-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            </div>    
+            <div className="col-2"><b>{props.digitFour}</b></div>
+            <div className="col-2"><b>{props.digitThree}</b></div>
+            <div className="col-2"><b>{props.digitTwo}</b></div>
+            <div className="col-2"><b>{props.digitOne}</b></div>
             </div>
         </div>
     );

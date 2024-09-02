da8f3246a8608c24fe779801f69780798d8a9057
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-09-02 23:51:30 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -29,27 +29,22 @@ const Home=(props)=>{
 }
 
 Home.propTypes = {
-    digitSix: PropTypes.number,
-    digitFive: PropTypes.number,
-    digitFour: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitOne: PropTypes.number
+    digitSix: PropTypes.string,
+    digitFive: PropTypes.string,
+    digitFour: PropTypes.string,
+    digitThree: PropTypes.string,
+    digitTwo: PropTypes.string,
+    digitOne: PropTypes.string
   };
 
 let counter=0;
 const domNode = document.getElementById('app');
 const root = ReactDOM.createRoot(domNode);
-const num = [0,0,0,0,0,0];
-function RestNumber(number){
-   let res =number.toString().split("").reverse();
-    for(let i =0; i<res.length;i++)
-        num[i]=parseInt(res[i]); 
-}
 setInterval(function(){
-   RestNumber(counter);
+   let res =counter.toString().split("").reverse();
     counter++;
-    root.render(<Home digitOne={num[0]} digitTwo={num[1]} digitThree={num[2]} digitFour={num[3]}   digitFive={num[4]} digitSix={num[5]}  />);
+    root.render(<Home digitOne={res[0]!=null?res[0]:"0"} digitTwo={res[1]!=null?res[1]:"0"} digitThree={res[2]!=null?res[2]:"0"} digitFour={res[3]!=null?res[3]:"0"}   digitFive={res[4]!=null?res[4]:"0"} digitSix={res[5]!=null?res[5]:"0"}  />);
+   // root.render(<Home digitOne={num[0]} digitTwo={num[1]} digitThree={num[2]} digitFour={num[3]}   digitFive={num[4]} digitSix={num[5]}  />);
 },1000);
 
 //render your react application
